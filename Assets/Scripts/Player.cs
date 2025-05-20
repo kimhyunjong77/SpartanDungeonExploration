272eece46ab0c3b52bf1bcfff1eb5333478c2243
kimhyunjong77 <rlagus0407@gmail.com>
2025-05-20 19:53:41 +0900

    이동, 점프, 카메라
    wasd로 이동, Space로 점프, 마우스로 카메라변경

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerController controller;
+
+    private void Awake()
+    {
+        CharacterManager.Instance.Player = this;
+        controller = GetComponent<PlayerController>();
+    }
+}
