d59fc7398b80a331c40db62b47f058a6aadb9406
kimhyunjong77 <rlagus0407@gmail.com>
2025-05-22 19:16:07 +0900

    Add : 점프 버섯
    점프대로 버섯을 추가했습니다

---
+++ Assets/Scripts/JumpMushroom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpMushroom : MonoBehaviour
+{
+    public float jumpForce = 100f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            Vector3 velocity = rb.velocity;
+            velocity.y = 0;
+            rb.velocity = velocity;
+
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
